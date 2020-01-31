2fe1bb51250b90d5b4df35611d9607752be28162
Arjen Simons <arjen.simons@gmail.com>
2020-01-31 22:17:47 +0100

    Added the progression manager script

---
+++ GGJ2020/Assets/Scripts/ProgressManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressManager : MonoBehaviour
+{
+    [SerializeField] private int maxSpeed;
+    [SerializeField] private int minSpeed;
+    [SerializeField] 
+}
