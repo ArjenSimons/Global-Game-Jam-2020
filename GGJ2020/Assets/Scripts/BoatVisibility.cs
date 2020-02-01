f27d238064dea1289672b6632ae20ed83dc5b5b4
LAPTOP-F5CBGI8R\stend <boers14@hva.nl>
2020-02-01 11:44:45 +0100

    Fake boat script added

---
+++ GGJ2020/Assets/Scripts/BoatVisibility.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatVisibility : MonoBehaviour
+{
+    [SerializeField]
+    private ProgressManager progressManager;
+
+    [SerializeField]
+    private GameObject enemyBoat, fakeEnemyBoat;
+
+    [SerializeField]
+    private Camera cam;
+
+    private BoatType boatType, enemyBoatType;
+
+    private Vector3 fakeBoatPos;
+
+    private float completeCamView, startPosCameraView, percentageEqualToPlayer, percentage, fakeBoatPosX, playerProgression, enemyProgression;
+
+    [SerializeField]
+    private float visibilityRange = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        completeCamView = cam.farClipPlane / 23.25f;
+        boatType = GetComponent<BoatType>();
+        enemyBoatType = enemyBoat.GetComponent<BoatType>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        checkBoatDistance();
+    }
+
+    private void checkBoatDistance()
+    {
+        playerProgression = progressManager.getProgression(boatType);
+        enemyProgression = progressManager.getProgression(enemyBoatType);
+
+        if (enemyProgression > playerProgression - visibilityRange && enemyProgression < playerProgression + visibilityRange)
+        {
+            showBoat(enemyProgression, playerProgression);
+        } 
+    }
+
+    private void showBoat(float enemyPos, float playerPos)
+    {
+        startPosCameraView = transform.position.x - (completeCamView / 2);
+        percentageEqualToPlayer = visibilityRange + (enemyPos - playerPos);
+        percentage = percentageEqualToPlayer / (visibilityRange * 2);
+        fakeBoatPosX = completeCamView * percentage;
+
+        fakeBoatPos = fakeEnemyBoat.transform.position;
+        fakeBoatPos.x = startPosCameraView + fakeBoatPosX;
+        fakeEnemyBoat.transform.position = fakeBoatPos;
+    }
+}
