8a35f8a715bead74cddc08687fd734f27ec0a156
cquang <cindy.quang@hva.nl>
2020-02-01 11:25:14 +0100

    some extra scripts to distingiush player for sequence puzzle

---
+++ GGJ2020/Assets/Scripts/ButtonSequence/SequencePuzzlePlayer2.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  This script manages the sequence puzzle
+/// </summary>
+public class SequencePuzzlePlayer2 : MonoBehaviour
+{
+    public enum ButtonsPlayer2 { A, B, X, Y }
+
+    [SerializeField]
+    private ButtonsPlayer2[] correctOrderSmall;
+
+    [SerializeField]
+    private ButtonsPlayer2[] correctOrderBig;
+
+    private int currentSequenceButton;
+    private int smallOrderMax;
+    private int bigOrderMax;
+
+    GameObject boatSegment;
+    BoatSegment bS;
+
+    public virtual void Start()
+    {
+        bS = GetComponentInChildren<BoatSegment>();
+
+        currentSequenceButton = 0;
+        smallOrderMax = 4;
+        bigOrderMax = 8;
+
+        SequenceRandomizer();
+    }
+
+
+    public virtual void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    Check(Buttons.A);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.B))
+        //{
+        //    Check(Buttons.B);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.X))
+        //{
+        //    Check(Buttons.X);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.Y))
+        //{
+        //    Check(Buttons.Y);
+
+
+        if (Input.GetButtonDown("A-Button2"))
+        {
+            Check(ButtonsPlayer2.A);
+            Debug.Log("press A logitech controller");
+        }
+
+        if (Input.GetButtonDown("B-Button2"))
+        {
+            Check(ButtonsPlayer2.B);
+            Debug.Log("press B logitech controller");
+        }
+
+        if (Input.GetButtonDown("X-Button2"))
+        {
+            Check(ButtonsPlayer2.X);
+            Debug.Log("press X logitech controller");
+        }
+        if (Input.GetButtonDown("Y-Button2"))
+        {
+            Check(ButtonsPlayer2.Y);
+            Debug.Log("press Y logitech controller");
+        }
+
+
+    }
+
+
+    public virtual void SequenceRandomizer()
+    {
+        // decide whether to use correctOrderSmall or correctOrderBig
+        //if (bS.MyStatus == BoatSegment.Status.SmallDamage)
+        //{
+        for (int i = 0; i < correctOrderSmall.Length; i++)
+        {
+            correctOrderSmall[i] = (ButtonsPlayer2)UnityEngine.Random.Range(0, 3);
+        }
+        //}
+
+        //if (bS.MyStatus == BoatSegment.Status.BigDamage)
+        //{
+
+        //}
+        for (int j = 0; j < correctOrderBig.Length; j++)
+        {
+            correctOrderBig[j] = (ButtonsPlayer2)UnityEngine.Random.Range(0, 7);
+        }
+    }
+
+    /// <summary>
+    /// Checks the input of player with the current sequence button
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public virtual void Check(ButtonsPlayer2 input)
+    {
+        if (input == correctOrderSmall[currentSequenceButton])
+        {
+            currentSequenceButton++;
+            Debug.Log("solved a thing");
+            if (currentSequenceButton == smallOrderMax)
+            {
+                Debug.Log(" you just solved the whole damn puzzle!");
+                currentSequenceButton = 0;
+                //delete hole
+            }
+        }
+        else
+        {
+            Debug.Log("you fucked up xD");
+            currentSequenceButton = 0;
+            // generate a new sequence
+            SequenceRandomizer();
+        }
+    }
+}
