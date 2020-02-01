9b4c4e1aa4776a2cbb348c5fd4525940235014a2
Benjamin <batsvanderwolf@yahoo.com>
2020-02-01 14:43:41 +0100

    updated GameStateManager

---
+++ GGJ2020/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class GameStateManager : MonoBehaviour
+{
+    public static GameStateManager Instance { get; private set; }
+    public bool GameOver { get; private set; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void OnGameEnd()
+    {
+        GameOver = true;
+    }
+}
